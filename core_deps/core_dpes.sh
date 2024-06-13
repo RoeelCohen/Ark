32aaa8103d9285da0020c91ab3ff5cdaae95f439
Roee <roeelcohen@gmail.com>
2024-06-13 13:24:10 +0300

    fix packages install

--- core_deps/core_dpes.sh
+++ core_deps/core_dpes.sh
@@ -5,7 +5,7 @@ echo "Installing core dependencies 🧱"
     curl -sL "${COR_DEPS_DIR}/_brew.sh" | bash
     curl -sL "${COR_DEPS_DIR}/_x-code.sh" | bash
     curl -sL "${COR_DEPS_DIR}/_zsh.sh" | bash
-    curl -sL "${COR_DEPS_DIR}/_brew-packages.sh" | bash
+    curl -sL "${COR_DEPS_DIR}/_packages.sh" | bash
 }
 
 echo '✔ Finished installing peer dependencies successfully'
